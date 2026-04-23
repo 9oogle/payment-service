303ffab0a761fbf400239f2161542c79680f4dec
soyeonnan <thduszbzb12@gmail.com>
2026-04-23 12:21:30 +0900

    feat: Payment Service 초기 설정 - common-library 연동 밒 DB 설정

--- src/main/java/com/goggles/payment_service/PaymentServiceApplication.java
+++ src/main/java/com/goggles/payment_service/PaymentServiceApplication.java
@@ -1,9 +1,18 @@
 package com.goggles.payment_service;
 
+import com.goggles.config.event.EventConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@Import(EventConfig.class)
+@SpringBootApplication(scanBasePackages = {"com.goggles"})
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"com.goggles"})
+@EntityScan(basePackages = {"com.goggles"})
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
