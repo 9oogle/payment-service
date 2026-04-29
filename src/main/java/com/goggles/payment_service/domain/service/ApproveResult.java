af9337a2dffa7535b41891b4979f3241589cb853
soyeonnan <thduszbzb12@gmail.com>
2026-04-29 14:42:48 +0900

    refactor: 결과 class 생성

--- src/main/java/com/goggles/payment_service/domain/service/ApproveResult.java
+++ src/main/java/com/goggles/payment_service/domain/service/ApproveResult.java
@@ -1,6 +1,17 @@
 package com.goggles.payment_service.domain.service;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class ApproveResult {
 
-
+    private final boolean success;
+    private final String paymentId;
+    private final String failReason;
+    private final LocalDateTime approveAt;
+    private final String paymentLog;
 }
