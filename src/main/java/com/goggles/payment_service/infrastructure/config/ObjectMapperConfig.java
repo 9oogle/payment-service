b7180173d3114bee53abc271b39124fb9e432b27
soyeonnan <thduszbzb12@gmail.com>
2026-05-03 19:29:09 +0900

    fix: ObjactMapperConfig 수정

--- src/main/java/com/goggles/payment_service/infrastructure/config/ObjectMapperConfig.java
+++ src/main/java/com/goggles/payment_service/infrastructure/config/ObjectMapperConfig.java
@@ -3,17 +3,18 @@ package com.goggles.payment_service.infrastructure.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ObjectMapperConfig {
 
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return objectMapper;
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer(RestClient.Builder builder) {
+        return builder -> builder
+                .modules(new JavaTimeModule())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
